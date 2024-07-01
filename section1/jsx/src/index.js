e333288e70c7737805e8ca738b80c753391f0ef1
iurecastro <55067724+iurecastro@users.noreply.github.com>
2024-07-01 13:54:45 +0100

    Using variables in JSX

--- section1/jsx/src/index.js
+++ section1/jsx/src/index.js
@@ -5,8 +5,22 @@ const el = document.querySelector('#root');
 const root = ReactDOM.createRoot(el);
 
 
+
 const App = () => {
-  return <h1>Hello, JSX!</h1>;
-}
+
+  const name = 'John';
+
+  return (
+    //use div or <> empty JSX element </> to wrap  elements 
+
+  //to access variable use {variable_name}
+  <div>
+    <h1>Hello, JSX!</h1>
+    <p>I love React</p>
+    
+    <p>Hello {name}!</p>
+  </div>
+  );
+};
 
 root.render(<App />);
