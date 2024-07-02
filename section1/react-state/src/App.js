77e40b976294f01c1f8af96f3555ec3a72a6dd92
iurecastro <55067724+iurecastro@users.noreply.github.com>
2024-07-02 15:07:12 +0100

    React - State
    Use the useState() function to create a state and a function that changes the state.
    
    Always use setState() function to change the state.
    
    React rerenders the component when its state changes.

--- section1/react-state/src/App.js
+++ section1/react-state/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const increase = () => setCounter(counter + 1);
+  const decrease = () => setCounter(counter - 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Counter</h1>
       </header>
-    </div>
-  );
+
+      <main>
+        <p>{counter}</p>
+        <div>
+          <button onClick={increase}>+</button>
+
+          <button onClick={decrease}>-</button>
+        </div>
+      </main>
+    </>
+  )
+
+
+
 }
 
 export default App;
